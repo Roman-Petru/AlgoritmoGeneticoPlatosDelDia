f81f047c43ded8c9ed1d1b329eeb9eb53366f788
Roman-Petru <ropetru@hotmail.com>
2023-10-19 14:46:47 -0300

    Added graphic and recipes

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ from Individual import get_best_individual
 from Selection import get_population_after_selection
 from CrossOver import get_population_after_crossover
 from Mutation import get_population_after_mutation
-import numpy
+import matplotlib.pyplot as plt
 
 class Main:
     def run(self):
@@ -12,6 +12,8 @@ class Main:
         print("Running Genetic Algorithm")
         config = load_config()
         solutions = []
+        x_axis = []
+        x_marker = 0
 
         population = get_initial_population()
         best = get_best_individual(population)
@@ -19,15 +21,18 @@ class Main:
             population = get_population_after_selection(population)
             population = get_population_after_crossover(population)
             population = get_population_after_mutation(population)
+
             inds = []
             for individual in population:
                 inds.append(individual.fitness())
             print("All fitness of new population: ", inds)
-            print("Average: ", numpy.average(inds))
-
             print("Best individual of generation ", str(gen_number + 1), " : ")
+
             gen_best = get_best_individual(population)
             solutions.append(gen_best.fitness())
+            x_axis.append(x_marker)
+            x_marker = x_marker + 1
+
             gen_best.print()
             print("Fitness function value: ", str(gen_best.fitness()))
             if gen_best.fitness() > best.fitness():
@@ -35,6 +40,23 @@ class Main:
 
         print("Best of run Fitness value: ", best.fitness())
         print("Best of run Chromosome: ", best.number_of_dishes())
+
+        plt.figure(figsize=(12, 9))
+        plt.plot(x_axis, solutions)
+
+        # Add labels and title
+        plt.xlabel('Corridas')
+        plt.ylabel('Ganancia total')
+
+        title = ("Cantidad de generaciones: ", config['number_of_generations'], "Tasa de cruzamiento: ", config['crossover_rate'],
+                 "Función de cruzamiento: ", config['crossover_type'], "Probabilidad de mutacion: ", config['mutation_probability'])
+        plt.title(title)
+
+        subtitle = 'Mejor funcion de la corrida: ', best.fitness(), ' - genes del mejor: ',  best.number_of_dishes()
+        plt.suptitle(subtitle, fontsize=12, color='gray')
+
+        plt.grid(True)
+        plt.show()
         return
 
 
--- Recipes.py
+++ Recipes.py
@@ -10,13 +10,55 @@ recipes = {
     'Arroz con Pollo': {
         'pollo': 400,
         'arroz': 250,
-    }
+    },
+    'Pizza': {
+        'harina': 400,
+        'tomate': 150,
+        'queso': 150,
+    },
+    'Lasagna': {
+        'harina': 250,
+        'queso': 250,
+        'tomate': 150,
+        'carne': 300,
+    },
+    'Tacos de carne': {
+        'carne': 300,
+        'tomate': 150,
+        'harina': 250,
+    },
+    'Tacos de pollo': {
+        'pollo': 300,
+        'tomate': 150,
+        'harina': 250,
+    },
+    'Hamburguesa': {
+        'carne': 200,
+        'queso': 100,
+        'harina': 250,
+    },
+    'Fideos con bolognesa': {
+        'carne': 200,
+        'harina': 600,
+        'tomate': 250,
+    },
+    'Ceviche': {
+        'pescado': 350,
+        'tomate': 150
+    },
 }
 
 prices = {
     'Sushi': 5000,
     'Pollo Frito': 3000,
-    'Arroz con Pollo': 3000
+    'Arroz con Pollo': 3000,
+    'Pizza': 4000,
+    'Lasagna': 7500,
+    'Tacos de carne': 2500,
+    'Tacos de pollo': 2500,
+    'Hamburguesa': 3500,
+    'Fideos con bolognesa': 3500,
+    'Ceviche': 5500,
 }
 
 total_ingredients = {
@@ -24,4 +66,8 @@ total_ingredients = {
     'pescado': 1500,
     'pollo': 3000,
     'papa': 3000,
+    'harina': 2500,
+    'tomate': 3000,
+    'queso': 1000,
+    'carne': 1500,
 }
